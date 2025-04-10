2dac76e92c611ad36f2e21fbf46aedb24cb4b0ed
ManhCuong200 <tmanhcuong2k4@gmail.com>
2025-04-10 19:33:00 +0700

    Thay doi mau sac & thanh dieu huong menu-Van Trieu

---
+++ Ve_Amor_Dating_App-5050a0c403de7f41d3f3e389850c1c6473fabb9c/lib/features/authentication/screens/login/widgets/widget_imports.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:ve_amor_app/features/authentication/screens/signup/signup_imports.dart';
+import 'package:ve_amor_app/navigation_menu.dart';
+import 'package:ve_amor_app/utils/constants/colors.dart';
+import 'package:ve_amor_app/utils/constants/image_strings.dart';
+import 'package:ve_amor_app/utils/constants/sizes.dart';
+import 'package:ve_amor_app/utils/constants/text_strings.dart';
+import 'package:ve_amor_app/utils/helpers/helper_functions.dart';
+
+import '../../password_configuration/password_configuration_imports.dart';
+
+part 'login_header.dart';
+part 'login_form.dart';
---
+++ Ve_Amor_Dating_App-5050a0c403de7f41d3f3e389850c1c6473fabb9c/lib/features/authentication/screens/password_configuration/password_configuration_imports.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:ve_amor_app/features/authentication/screens/login/login_imports.dart';
+import 'package:ve_amor_app/utils/constants/image_strings.dart';
+import 'package:ve_amor_app/utils/constants/sizes.dart';
+import 'package:ve_amor_app/utils/constants/text_strings.dart';
+import 'package:ve_amor_app/utils/helpers/helper_functions.dart';
+
+
+part 'forget_password.dart';
+part 'reset_password.dart';
